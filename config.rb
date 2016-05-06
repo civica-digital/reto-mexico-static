8b1add1f78d6b44d677b4c203364a37a5e101d79
poguez <zeugop@gmail.com>
2016-05-06 11:28:39 -0500

    Added support for config file in a YAML for deploy

--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
 require "extensions/views"
-
+require 'yaml'
 activate :views
 activate :directory_indexes
 activate :autoprefixer
@@ -10,6 +10,13 @@ set :js_dir, "assets/javascripts"
 set :images_dir, "assets/images"
 set :fonts_dir, "assets/fonts"
 set :layout, "layouts/application"
+
+env_vars = begin
+   YAML.load_file('./env.yml')
+rescue ArgumentError => e
+  puts "Could not parse YAML: #{e.message}"
+  puts "Configure env.yml"
+end
 
 configure :development do
  activate :livereload
@@ -23,6 +30,24 @@ end
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.method = :git
+end
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = env_vars['AWS_BUCKET'] # The name of the S3 bucket you are targeting. This is globally unique.
+  s3_sync.region                     = 'us-east-1'     # The AWS region for your bucket.
+  s3_sync.aws_access_key_id          = env_vars['AWS_ACCESS_KEY_ID']
+  s3_sync.aws_secret_access_key      = env_vars['AWS_SECRET_ACCESS_KEY']
+  s3_sync.delete                     = false # We delete stray files by default.
+  s3_sync.after_build                = false # We do not chain after the build step by default.
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+  s3_sync.prefix                     = ''
+  s3_sync.version_bucket             = false
+  s3_sync.index_document             = 'index.html'
+  s3_sync.error_document             = '404.html'
 end
 
 helpers do
